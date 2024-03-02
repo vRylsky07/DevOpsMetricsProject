90cfb8c1799de1b98360f892ab9b4b3bcc3f3e90
vRylsky07 <vladimir.rylsky07@gmail.com>
2024-03-02 23:03:33 +0300

    add http:// part

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -112,7 +112,7 @@ func (sStg *SenderStorage) CreateMetricURL(mType constants.MetricType, mainURL s
 	case constants.CounterType:
 		mTypeString = "/counter/"
 	}
-	return mainURL + "/update" + mTypeString + name + "/" + strconv.FormatFloat(value, 'f', 3, 64)
+	return "http://" + mainURL + "/update" + mTypeString + name + "/" + strconv.FormatFloat(value, 'f', 3, 64)
 }
 
 func CreateSender() *SenderStorage {
